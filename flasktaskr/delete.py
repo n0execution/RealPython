3076eb9dfacc4ab51be8ae959f3dd2951e98a987
qwerty432 <dimichkit@ukr.net>
2017-12-13 02:14:32 +0200

    Revert "added flasktaskr"
    This reverts commit df5a3daa4ab5a29c4c8e4dbfd12218a987bac800.
    
    commit -m "revert"

--- flasktaskr/delete.py
+++
@@ -1,9 +0,0 @@
-from project._config import DATABASE_PATH
-
-import sqlite3
-from datetime import datetime
-
-with sqlite3.connect(DATABASE_PATH) as connection :
-    # get a cursor object used to execute SQL commands
-	cursor = connection.cursor()
-	cursor.execute("""DELETE FROM Users""")
