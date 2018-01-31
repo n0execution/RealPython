fb08c7f9a775f53888fe86c374baf791452f0ea0
qwerty432 <dimichkit@ukr.net>
2018-01-31 18:07:41 +0200

    appended the text to the DOM

--- frontend/main.js
+++ frontend/main.js
@@ -6,9 +6,14 @@ $(function() {
     $("#btn-click").click(function() 
     {
         if ($('input').val() !== '') 
+        // grab the value from the input box after the button click
         {
             var input = $("input").val()
+            // display value within the browser's JS console
             console.log(input)
+            // add the value to the DOM
+            $('ol').append('<li><a href="">x</a> - ' + input + '</li>');
         }
+        $('input').val('')
     });
 });
